67c64d2808b08a05da9b69d8deeb6fbab38953f1
Aurinko23 <evgesacymbalova5391@gmail.com>
2024-10-05 22:37:51 +0300

    after flake8

--- LifeCodeLabs.py
+++ LifeCodeLabs.py
@@ -1,8 +1,16 @@
 from Moduls.moduls_filter_fastq import check_quality, check_gc, check_lenght
-from Moduls.moduls_dna_rna_tools import transcribe, reverse, complement, reverse_complement, verification
+from Moduls.moduls_dna_rna_tools import (
+    transcribe,
+    reverse,
+    complement,
+    reverse_complement,
+    verification,
+)
 
 
-def filter_fastq(seqs, gc_bounds=(0, 100), length_bounds=(0, 2**32), quality_threshold=0):
+def filter_fastq(
+    seqs, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), quality_threshold=0
+):
 
     new_sq = check_quality(seqs, quality_threshold)
     seqs_after_gc = check_gc(new_sq, gc_bounds)
@@ -15,19 +23,19 @@ def run_dna_rna_tools(*args):
 
     verification(na)
 
-    if func_name == 'transcribe':
+    if func_name == "transcribe":
         func = transcribe
-    elif func_name == 'reverse':
+    elif func_name == "reverse":
         func = reverse
-    elif func_name == 'complement':
+    elif func_name == "complement":
         func = complement
-    elif func_name == 'reverse_complement':
+    elif func_name == "reverse_complement":
         func = reverse_complement
     else:
         print("Error")
 
     result = func(na)
     if len(result) == 1:
-        return (" ".join(result))
+        return " ".join(result)
     else:
         return result
--- Moduls/moduls_dna_rna_tools.py
+++ Moduls/moduls_dna_rna_tools.py
@@ -5,6 +5,7 @@ def verification(na):
                 print("Error input")
                 exit(0)
 
+
 def transcribe(*args):
     new_na = []
     for nucls in args:
@@ -12,6 +13,7 @@ def transcribe(*args):
             na_trans = nucl.replace("T", "U").replace("t", "u")
             new_na.append(na_trans)
     return new_na
+
 
 def reverse(*args):
     new_na = []
@@ -26,8 +28,30 @@ def complement(*args):
     new_na = []
     for nucls in args:
         for nucl in nucls:
-            change1 = nucl.replace("A", "O").replace("a", "o").replace("C", "H").replace("c", "h").replace("T", "D").replace("t", "d").replace("G", "K").replace("g", "k").replace("U", "J").replace("u", "j")
-            change2 = change1.replace("O", "T").replace("o", "t").replace("H", "G").replace("h", "g").replace("D", "A").replace("d", "a").replace("K", "C").replace("k", "c").replace("J", "A").replace("j", "a")
+            change1 = (
+                nucl.replace("A", "O")
+                .replace("a", "o")
+                .replace("C", "H")
+                .replace("c", "h")
+                .replace("T", "D")
+                .replace("t", "d")
+                .replace("G", "K")
+                .replace("g", "k")
+                .replace("U", "J")
+                .replace("u", "j")
+            )
+            change2 = (
+                change1.replace("O", "T")
+                .replace("o", "t")
+                .replace("H", "G")
+                .replace("h", "g")
+                .replace("D", "A")
+                .replace("d", "a")
+                .replace("K", "C")
+                .replace("k", "c")
+                .replace("J", "A")
+                .replace("j", "a")
+            )
             new_na.append(change2)
     return new_na
 
